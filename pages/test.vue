14c488976c6b26b66d6a1e97da2651854b47584f
Simon de Bruijn <simon.debruijn@publiq.be>
2021-06-24 14:42:47 +0200

    Use &:& example code to test

--- pages/test.vue
+++ pages/test.vue
@@ -5,22 +5,22 @@
       <input type="email" name="email" id="email" />
       <label for="agree">Agree</label>
       <input type="checkbox" name="agree" id="agree" />
-      <vue-hcaptcha
-        ref="invisibleHcaptcha"
-        :sitekey="hcaptchaKey"
-        size="invisible"
-        theme="dark"
-        @verify="onVerify"
-        @expired="onExpire"
-        @challengeExpired="onExpire"
-        @error="onError"
-      />
       <button type="submit">Subscribe</button>
       <small>
         This site is protected by hCaptcha and its
         <a href="https://hcaptcha.com/privacy">Privacy Policy</a> and
         <a href="https://hcaptcha.com/terms">Terms of Service</a> apply.
       </small>
+      <vue-hcaptcha
+        ref="invisibleHcaptcha"
+        :sitekey="hcaptchaKey"
+        language="nl"
+        data-size="invisible"
+        @verify="onVerify"
+        @expired="onExpire"
+        @challengeExpired="onExpire"
+        @error="onError"
+      ></vue-hcaptcha>
     </form>
   </div>
 </template>
@@ -30,32 +30,52 @@ import VueHcaptcha from '@hcaptcha/vue-hcaptcha';
 
 export default {
   components: { VueHcaptcha },
+  data() {
+    return {
+      verified: false,
+      token: null,
+      key: null,
+      hcaptchaSuccess: false,
+    };
+  },
   methods: {
-    onVerify(token, ekey) {
+    async onVerify(token, key) {
       this.verified = true;
       this.token = token;
-      this.eKey = ekey;
-      console.log(`Callback token: ${token}, ekey: ${ekey}`);
+      this.key = key;
+
+      const response = await fetch('hcaptcha/verify', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ token, key }),
+      });
+
+      const { success } = await response.json();
+
+      this.hcaptchaSuccess = success;
+
+      if (success) {
+        alert("You're good to go");
+      }
     },
     onExpire() {
       this.verified = false;
       this.token = null;
-      this.eKey = null;
+      this.key = null;
       this.expired = true;
       console.log('Expired');
     },
     onError(err) {
       this.token = null;
-      this.eKey = null;
+      this.key = null;
       this.error = err;
       console.log(`Error: ${err}`);
     },
     onSubmit() {
-      console.log(
-        'Submitting the invisible hCaptcha',
-        this.$refs.invisibleHcaptcha
-      );
       this.$refs.invisibleHcaptcha.execute();
+      if (this.hcaptchaSuccess) {
+        console.log('you can sumbit');
+      }
     },
   },
   computed: {
